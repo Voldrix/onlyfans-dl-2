033687cde2cca0943db50d8f0fa2e8fc58e8ebcb
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-06-28 20:29:21 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -92,6 +92,7 @@ def split_video_with_ffmpeg(input_file, output_file, start_time, duration):
     current_split_process.wait()  # Wait for the process to finish
 
 async def split_and_send_large_file(chat_id, file_path, tag):
+    global current_split_process
     video = VideoFileClip(file_path)
     duration = video.duration
     file_size = os.path.getsize(file_path)
@@ -112,14 +113,152 @@ async def split_and_send_large_file(chat_id, file_path, tag):
             '-c:v', 'libx264', '-preset', 'ultrafast', '-threads', '4', '-c:a', 'aac', part_path
         ]
 
-        global current_split_process
         current_split_process = subprocess.Popen(command)
         current_split_process.wait()  # Wait for the process to finish
+
+        if current_split_process.returncode != 0:  # Check if the process was terminated
+            await client.send_message(chat_id, "Splitting process was stopped.")
+            break
 
         await send_file_and_replace_with_empty(chat_id, part_path, f"{tag} Part {i + 1}")
         os.remove(part_path)  # Remove part file after sending
 
+    # Replace original large file with an empty file or remove it
+    if delete_media_from_server:
+        with open(file_path, 'w') as f:
+            pass  # Open in write mode to make file empty
+    else:
+        os.remove(file_path)  # Remove the original large file
+
     current_split_process = None  # Reset the process variable
+
+
+
+async def process_large_file(profile_dir, file_path, chat_id, tag, pinned_message_id):
+    try:
+        await split_and_send_large_file(chat_id, file_path, tag)
+        if delete_media_from_server:
+            with open(file_path, 'w') as f:
+                pass  # Open in write mode to make file empty
+        else:
+            os.remove(file_path)
+    except MessageNotModifiedError:
+        pass
+
+
+@client.on(events.NewMessage(pattern='/get_big$'))
+async def get_big_command_usage(event):
+    if event.sender_id == TELEGRAM_USER_ID:
+        msg = await event.respond("Usage: /get_big <username or subscription number>")
+        TEXT_MESSAGES.append(msg.id)
+
+@client.on(events.NewMessage(pattern='/get_big (.+)'))
+async def get_big_command(event):
+    if event.sender_id != TELEGRAM_USER_ID:
+        msg = await event.respond("Unauthorized access.")
+        USER_MESSAGES.append(msg.id)
+        logger.warning(f"Unauthorized access denied for {event.sender_id}.")
+        return
+
+    args = event.pattern_match.group(1).strip().split()
+    target = args[0]
+    max_age = int(args[1]) if len(args) > 1 and args[1].isdigit() else 0
+    tag = f"#{target}"
+
+    try:
+        with open("subscriptions_list.txt", "r") as f:
+            subscriptions = f.readlines()
+
+        if target.isdigit():
+            target_index = int(target) - 1
+            if target_index < 0 or target_index >= len(subscriptions):
+                raise IndexError
+            username = subscriptions[target_index].strip()
+            tag = f"#{username}"
+        else:
+            username = target
+            tag = f"#{target}"
+
+        if username not in [sub.strip() for sub in subscriptions]:
+            msg = await event.respond(f"User {username} not found in the subscriptions list. {tag}")
+            USER_MESSAGES.append(msg.id)
+            return
+    except (IndexError, FileNotFoundError):
+        msg = await event.respond("Invalid subscription number or subscriptions list not found.")
+        USER_MESSAGES.append(msg.id)
+        return
+
+    if not os.path.exists(username):
+        os.makedirs(username)
+        msg = await event.respond(f"User directory {username} not found. Starting a fresh download. {tag}")
+        USER_MESSAGES.append(msg.id)
+
+    try:
+        pinned_message = await event.respond(f"Started downloading large media for {username} {tag}")
+        TEXT_MESSAGES.append(pinned_message.id)
+        pinned_message_id = pinned_message.id
+        await client(UpdatePinnedMessageRequest(
+            peer=event.chat_id,
+            id=pinned_message_id,
+            silent=True
+        ))
+
+        await download_and_send_large_media(username, event.chat_id, tag, pinned_message_id, max_age, event)
+    except FloodWaitError as e:
+        wait_time = e.seconds
+        await handle_flood_wait_error(event, wait_time)
+    except Exception as e:
+        await event.respond(f"Unexpected error occurred: {str(e)}")
+
+async def download_and_send_large_media(username, chat_id, tag, pinned_message_id, max_age, event):
+    profile_dir = username
+    large_files = []
+    tasks = []
+
+    command = ['python3', ONLYFANS_DL_SCRIPT, username, str(max_age)]
+    global current_split_process
+    current_split_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    processes[chat_id] = current_split_process
+
+    while True:
+        output = current_split_process.stdout.readline().strip()
+        if not output and current_split_process.poll() is not None:
+            break
+        if output:
+            logger.info(output)
+            if "Downloaded" in output and "new" in output:
+                for dirpath, _, filenames in os.walk(profile_dir):
+                    for filename in filenames:
+                        file_path = os.path.join(dirpath, filename)
+                        if not filename.endswith('.part') and os.path.getsize(file_path) > 0 and file_path not in large_files and 'sent_files.txt' not in file_path:
+                            if os.path.getsize(file_path) > TELEGRAM_FILE_SIZE_LIMIT:
+                                large_files.append(file_path)
+
+    current_split_process.stdout.close()
+    current_split_process.stderr.close()
+    current_split_process = None
+    del processes[chat_id]
+
+    if not large_files:
+        msg = await client.send_message(chat_id, f"No large files found for this user. {tag}")
+        TEXT_MESSAGES.append(msg.id)
+        return
+
+    download_complete_msg = await client.send_message(chat_id, f"Download complete. {tag}")
+    TEXT_MESSAGES.append(download_complete_msg.id)
+
+    semaphore = asyncio.Semaphore(MAX_PARALLEL_UPLOADS)
+    lock = asyncio.Lock()  # Создайте объект Lock для синхронизации
+    remaining_files = [len(large_files)]  # Используйте список для изменяемого объекта
+
+    for file_path in large_files:
+        tasks.append(upload_with_semaphore(semaphore, process_large_file, profile_dir, file_path, chat_id, tag, pinned_message_id))
+
+    await asyncio.gather(*tasks)
+
+    # inform user in chat that upload is complete
+    upload_complete_msg = await client.send_message(chat_id, f"Upload of large files complete. {tag}")
+    TEXT_MESSAGES.append(upload_complete_msg.id)
 
 
 
@@ -228,6 +367,7 @@ async def download_and_send_media(username, chat_id, tag, pinned_message_id, max
     profile_dir = username
     new_files = []
     total_files = 0
+    large_files = []
     tasks = []
 
     estimated_size = estimate_download_size(profile_dir)
@@ -251,8 +391,11 @@ async def download_and_send_media(username, chat_id, tag, pinned_message_id, max
                     for filename in filenames:
                         file_path = os.path.join(dirpath, filename)
                         if not filename.endswith('.part') and os.path.getsize(file_path) > 0 and file_path not in new_files and 'sent_files.txt' not in file_path:
-                            new_files.append(file_path)
-                            total_files += 1
+                            if os.path.getsize(file_path) <= TELEGRAM_FILE_SIZE_LIMIT:
+                                new_files.append(file_path)
+                                total_files += 1
+                            else:
+                                large_files.append(file_path)
 
     current_split_process.stdout.close()
     current_split_process.stderr.close()
@@ -262,9 +405,10 @@ async def download_and_send_media(username, chat_id, tag, pinned_message_id, max
     # Filter out already sent files
     sent_files = load_sent_files(profile_dir)
     new_files = [file for file in new_files if os.path.basename(file) not in sent_files]
+    large_files = [file for file in large_files if os.path.basename(file) not in sent_files]
     total_files = len(new_files)
 
-    if not new_files:
+    if not new_files and not large_files:
         msg = await client.send_message(chat_id, f"No new photos or videos found for this user. {tag}")
         TEXT_MESSAGES.append(msg.id)
         return
@@ -293,14 +437,22 @@ async def download_and_send_media(username, chat_id, tag, pinned_message_id, max
 
     await asyncio.gather(*tasks)
 
+    # Notify about large files
+    for file_path in large_files:
+        file_name = os.path.basename(file_path)
+        msg = await client.send_message(chat_id, f"Large file detected: {file_name}. Use /get_big to download.")
+        TEXT_MESSAGES.append(msg.id)
+
     # inform user in chat that upload is complete
     upload_complete_msg = await client.send_message(chat_id, f"Upload complete. {tag}")
     TEXT_MESSAGES.append(upload_complete_msg.id)
 
 
-async def upload_with_semaphore(semaphore, process_file, profile_dir, file_path, chat_id, tag, pinned_message_id, remaining_files, lock):
+
+async def upload_with_semaphore(semaphore, process_file, *args):
     async with semaphore:
-        await process_file(profile_dir, file_path, chat_id, tag, pinned_message_id, remaining_files, lock)
+        await process_file(*args)
+
 
 async def download_media_without_sending(username, chat_id, tag, max_age):
     profile_dir = username
@@ -725,7 +877,9 @@ async def stop_command(event):
             current_split_process.terminate()
             current_split_process.wait()
             current_split_process = None
-        await event.respond("Splitting process stopped.")
+            await event.respond("Splitting process stopped.")
+        else:
+            await event.respond("No splitting process running.")
         # Restart the bot by restarting the script
         os.system("pkill -f telegram_bot.py")  # Kills the script
         os.system("python3 telegram_bot.py &")  # Restarts the script
@@ -782,6 +936,7 @@ async def setup_bot_commands():
     commands = [
         BotCommand(command='list', description='Show list of active subscriptions'),
         BotCommand(command='get', description='Download media and send to this chat'),
+        BotCommand(command='get_big', description='Download and send large media files'),
         BotCommand(command='load', description='Download media to server without sending'),
         BotCommand(command='check', description='Check downloaded profiles and media count'),
         BotCommand(command='erase', description='Erase chat messages with a specific hashtag'),
@@ -792,8 +947,10 @@ async def setup_bot_commands():
         BotCommand(command='user_agent', description='Update USER_AGENT'),
         BotCommand(command='x_bc', description='Update X_BC'),
         BotCommand(command='sess_cookie', description='Update SESS_COOKIE')
+
     ]
     await client(SetBotCommandsRequest(scope=BotCommandScopeDefault(), lang_code='', commands=commands))
+
 
 def main():
     loop = asyncio.get_event_loop()
