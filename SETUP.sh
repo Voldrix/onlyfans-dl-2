b3fc1552a096c4f29be5d932336d5b4f9d665fd9
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-07-04 19:05:08 +0300

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -5,7 +5,12 @@ read -p "Enter OnlyFans USER_ID: " USER_ID
 read -p "Enter OnlyFans USER_AGENT: " USER_AGENT
 read -p "Enter OnlyFans X_BC: " X_BC
 read -p "Enter OnlyFans SESS_COOKIE: " SESS_COOKIE
-read -p "Enter 2 Botfather TOKEN (comma-separated): " TELEGRAM_BOT_TOKENS
+
+# Prompt for Botfather tokens
+read -p "Enter the first Botfather TOKEN: " BOT_TOKEN1
+read -p "Enter the second Botfather TOKEN: " BOT_TOKEN2
+
+# Prompt for Telegram App API details
 read -p "Enter Telegram App API_ID: " API_ID
 read -p "Enter Telegram App API_HASH: " API_HASH
 read -p "Enter your TELEGRAM_USER_ID: " TELEGRAM_USER_ID
@@ -16,7 +21,7 @@ USER_ID = "$USER_ID"
 USER_AGENT = "$USER_AGENT"
 X_BC = "$X_BC"
 SESS_COOKIE = "$SESS_COOKIE"
-TELEGRAM_BOT_TOKENS = [$TELEGRAM_BOT_TOKENS]
+TELEGRAM_BOT_TOKENS = ["$BOT_TOKEN1", "$BOT_TOKEN2"]
 API_ID = '$API_ID'
 API_HASH = '$API_HASH'
 TELEGRAM_USER_ID = $TELEGRAM_USER_ID
