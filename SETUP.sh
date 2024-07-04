99bcbbebfd46c96741ad27cef353bebff47dca2f
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-07-04 11:05:19 +0300

    Update SETUP.sh

--- SETUP.sh
+++ SETUP.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Prompt for user input and update user_config.py
-read -p "Enter USER_ID: " USER_ID
-read -p "Enter USER_AGENT: " USER_AGENT
-read -p "Enter X_BC: " X_BC
-read -p "Enter SESS_COOKIE: " SESS_COOKIE
-read -p "Enter TELEGRAM_BOT_TOKENS (comma-separated): " TELEGRAM_BOT_TOKENS
-read -p "Enter API_ID: " API_ID
-read -p "Enter API_HASH: " API_HASH
-read -p "Enter TELEGRAM_USER_ID: " TELEGRAM_USER_ID
+read -p "Enter OnlyFans USER_ID: " USER_ID
+read -p "Enter OnlyFans USER_AGENT: " USER_AGENT
+read -p "Enter OnlyFans X_BC: " X_BC
+read -p "Enter OnlyFans SESS_COOKIE: " SESS_COOKIE
+read -p "Enter 2 Botfather TOKEN (comma-separated): " TELEGRAM_BOT_TOKENS
+read -p "Enter Telegram App API_ID: " API_ID
+read -p "Enter Telegram App API_HASH: " API_HASH
+read -p "Enter your TELEGRAM_USER_ID: " TELEGRAM_USER_ID
 
 # Create or update user_config.py with the provided values
 cat <<EOL > user_config.py
