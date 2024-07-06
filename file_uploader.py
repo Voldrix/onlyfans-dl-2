d16233e31d33f9cf5a717585ee96a18e10591c9e
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-07-06 19:30:43 +0300

    Update file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -9,6 +9,7 @@ import subprocess
 import logging
 from PIL import Image
 from moviepy.editor import VideoFileClip
+from telethon.tl.types import DocumentAttributeVideo
 from telethon.tl.types import InputMediaUploadedPhoto, InputPhoto
 from telethon.tl.types import InputMediaUploadedDocument
 from telethon.errors.rpcerrorlist import FloodWaitError, MessageNotModifiedError
@@ -183,9 +184,13 @@ async def process_video_batch(profile_dir, video_batch, chat_id, tag, pinned_mes
                 os.remove(file_path)
                 continue
 
-            # Загружаем видео на сервер Telegram и получаем объект InputMediaUploadedDocument
+            # Загружаем видео на сервер Telegram и получаем объект InputFile
             uploaded_video = await client.upload_file(file_path)
-            media_group.append(InputMediaUploadedDocument(file=uploaded_video))
+            media_group.append(types.InputMediaUploadedDocument(
+                file=uploaded_video,
+                mime_type='video/mp4',
+                attributes=[DocumentAttributeVideo(duration=0, w=0, h=0)]
+            ))
             post_date = os.path.basename(file_path).split('_')[0]
             captions.append(f"{i + 1}. {post_date}")
 
