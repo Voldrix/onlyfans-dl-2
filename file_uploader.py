5ec5d9f77e545640c354343ec47a12ee47bc5a08
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-07-06 22:22:30 +0300

    Update file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -180,8 +180,8 @@ async def process_photo_batch(profile_dir, photo_batch, chat_id, tag, pinned_mes
     except Exception as e:
         logger.error(f"Failed to process photo batch: {str(e)}")
         
-#new8
-from telethon.tl.types import DocumentAttributeVideo, InputMediaDocument, InputSingleMedia
+#new9
+from telethon.tl.types import DocumentAttributeVideo, InputMediaUploadedDocument, InputSingleMedia
 
 async def process_video_batch(profile_dir, video_batch, chat_id, tag, pinned_message_id, remaining_files_ref, lock, client):
     try:
@@ -195,10 +195,8 @@ async def process_video_batch(profile_dir, video_batch, chat_id, tag, pinned_mes
 
             # Загружаем видео на сервер Telegram и получаем объект InputFile
             uploaded_video = await client.upload_file(file_path)
-            media = InputMediaDocument(
-                id=uploaded_video.id,
-                access_hash=uploaded_video.access_hash,
-                file_reference=uploaded_video.file_reference,
+            media = InputMediaUploadedDocument(
+                file=uploaded_video,
                 mime_type='video/mp4',
                 attributes=[DocumentAttributeVideo(duration=0, w=0, h=0)]
             )
